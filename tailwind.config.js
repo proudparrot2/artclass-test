62702a6f105c072cdee5a676791b3a16903c6131
FieryNinja10 <zarar_ahmed@outlook.com>
2023-02-22 11:02:31 -0500

    tailwind installed with CLI and footer responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.html", "./src/**/*.html", "*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darker: "rgba(0, 0, 0, 0.2)",
+        darkBlue: "#084277",
+        shadowBlue: "#0074BB",
+        sweetBlue: "#35AAF2",
+        niceWhite: "#F9FAFB",
+        lightLime: "#3AD97B",
+        lightGrey: "#f7f7f7",
+      },
+      fontFamily: {
+        dmSans: "'DM Sans', sans-serif",
+        silkscreen: "'Silkscreen', cursive",
+      },
+    },
   },
   plugins: [],
-}
+};
