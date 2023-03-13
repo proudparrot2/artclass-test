cf261170855b335ee6e2390646285825de7e38a5
FieryNinja10 <zarar_ahmed@outlook.com>
2023-03-13 18:33:31 -0400

    contact-form-action added back

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-let contactForm = document.getElementById("contactForm");
-
-console.log(process.env.PUBLIC_FORMSUBMIT_ACTION_LINK);
-contactForm.setAttribute("action", process.env.PUBLIC_FORMSUBMIT_ACTION_LINK);
